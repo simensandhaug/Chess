337526fc9a13f7487072735f3f90251209c3437f
Simen Sandhaug <simen.sandhaug@gmail.com>
2023-01-19 23:41:40 +0100

    update module-info.java

--- chess/src/main/java/module-info.java
+++ chess/src/main/java/module-info.java
@@ -3,12 +3,17 @@ module com.chess {
     requires javafx.fxml;
 
     opens com.chess to javafx.fxml;
-    opens com.chess.controller to javafx.fxml;
-    opens com.chess.view to javafx.fxml;
-    opens com.chess.model to javafx.fxml;
+    opens com.chess.controllers to javafx.fxml;
+    opens com.chess.views to javafx.fxml;
+    opens com.chess.models to javafx.fxml;
 
-    exports com.chess.controller;
-    exports com.chess.view;
-    exports com.chess.model;
+    exports com.chess.controllers;
+    exports com.chess.views;
+    exports com.chess.models;
+    exports com.chess.common;
+    exports com.chess.models.board;
+    exports com.chess.models.pieces;
+    exports com.chess.models.squares;
+    exports com.chess.models.game;
     exports com.chess;
 }
