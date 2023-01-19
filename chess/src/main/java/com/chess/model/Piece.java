64033fdee4e1fb94218976291ac3d586ae5ec90b
Simen Sandhaug <simen.sandhaug@gmail.com>
2023-01-19 19:10:14 +0100

    add value to pieces

--- chess/src/main/java/com/chess/model/Piece.java
+++ chess/src/main/java/com/chess/model/Piece.java
@@ -3,14 +3,14 @@ package com.chess.model;
 public abstract class Piece {
     private String name;
     private boolean color;
-    private int[] position;
+    private int[] position = new int[2];
     private int value;
 
     public Piece(String name, boolean color, int[] position) {
         this.name = name;
         this.color = color;
         this.position = position;
-        this.value = getValue();
+        this.value = 0;
     }
 
     public String getName() {
@@ -25,11 +25,13 @@ public abstract class Piece {
         return position;
     }
 
+    public int getValue() {
+        return value;
+    }
+
     public void setPosition(int[] newPosition) {
         this.position = newPosition;
     }
 
     public abstract boolean isMoveValid(int[] newPosition);
-
-    public abstract int getValue();
 }
